8eadb33693b2cf5ffeae2c7fcba8d614667519be
hannahfierro <224HFierro@frhsd.com>
2024-01-25 09:01:30 -0500

    update finished pictures

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(/* function */ callback, /* DOMElement */ element){
+			  window.setTimeout(callback, 1000 / 60);
+			};
+  })();
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,34 +18,31 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+  requestAnimFrame( animate );
+  var currentTime = new Date().getTime();
+  if (mLastFrameTime === 0) {
+	  mLastFrameTime = currentTime;
+  }
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+  if ((currentTime - mLastFrameTime) > mWaitTime) {
+	  swapPhoto();
+	  mLastFrameTime = currentTime;
+  }
 }
-
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	
-	if(mCurrentIndex >= mImages.length)
-	{
+	if(mCurrentIndex >= mImages.length){
 		mCurrentIndex = 0;
 	}
-	
-	if(mCurrentIndex < 0) {
-		mCurrentIndex = mImages.length-1;
+  
+	if(mCurrentIndex < 0){
+		mCurrentIndex = mImages.length - 1;
 	}
 	
-	document.getElementById('photo').src= mImages[mCurrentIndex].img;
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
 	var loc = document.getElementsByClassName('location');
-	loc[0].innerHTML="Location: "+ mImages[mCurrentIndex].location;
+	loc[0].innerHTML= "Location: "+ mImages[mCurrentIndex].location;
 	var des = document.getElementsByClassName('description');
 	des[0].innerHTML = "Description: " + mImages[mCurrentIndex].description;
 	var dt = document.getElementsByClassName('date');
@@ -56,19 +53,7 @@ function swapPhoto() {
 }
 
 
-function toggleDeatails()
-{
-	if($(".moreIndicator").hasClass("rot90"))
-	{
-		$( ".moreIndicator" ).removeClass("rot90");
-		$(".moreInicator" ).addclass("rot270");
-		}
-		else {
-			$(".moreIndicator" ).removeClass("rot 270");
-			$(".moreIndicator").addClass("rot90");
-		}
-		$(".details" ).slideToggle("slow", "linear");
-	}
+
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
@@ -83,38 +68,8 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl;
+var mUrl= '/images.json';
 
-
-
-
-function fetchJSON()
-{
-	mRequest.onreadystatechange = function() {
-		console.log("on ready state change");
-		if(this.readyState == 4 && this.status == 200) {
-			mJSON = JSON.parse(mRequest.responseText);
-			iterateJSON(mJson);
-		}
-	}
-	mRequest.open("GET", mUrl,true);
-	mRequest.send();
-}
-
-
-
-//Part 2 Slideshow 3
-function iterateJSON(mJson)
-{
-	for( x = 0; x < mJson.images.length; x++)
-{
-    mImages[x] = new GalleryImage();
-    mImages[x].location = mJson.images[x].imgLocation;
-    mImages[x].description = mJson.images[x].description;
-    mImages[x].date = mJson.images[x].date;
-    mImages[x].img = mJson.images[x].imgPath;
-}
-}
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
@@ -122,44 +77,36 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 		galleryImage.img = e.target;
 		mImages.push(galleryImage);
 	}
-}
+  }
 
 $(document).ready( function() {
-	
+  
 	// This initially hides the photos' metadata information
 	// $('.details').eq(0).hide();
-	$( "nextPhoto" ).position({
+	$("#nextPhoto").position({
 		my: "right bottom",
 		at: "right bottom",
 		of: "#nav"
-});
-
+	});
+	
+  });
 
 const urlParams = new URLSearchParams(window.location.search);
 
-for (const [key, value] of urlParams) {
-	console.log(`${key}:${value}`);
-	mUrl = value;
+for(const [key, value] of urlParams){
+  console.log(`${key}:${value}`);
+  mUrl=value;
 }
-if(mUrl == undefined)
-{
-  	mUrl = 'images.json';
+if(mUrl == undefined){
+  mUrl = '/images.json';
 }
 
 
 fetchJSON();
 
-// This initially hides the photos' metadata information
-// $('.details').eq(0).hide();
-
-});
-
-
-
-
 window.addEventListener('load', function() {
-	
-	console.log('window loaded');
+  
+  console.log('window loaded');
 
 }, false);
 
@@ -174,3 +121,40 @@ function GalleryImage() {
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
 var img;
 }
+
+//Part 2 Slideshow 3
+function iterateJSON(){
+	for(x = 0; x < mJson.images.length; x++){
+		mImages[x] = new GalleryImage;
+		mImages[x].location = mJson.images[x].imgLocation;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+	}
+  }
+
+
+function fetchJSON(){
+	mRequest.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			mJson = JSON.parse(mRequest.responseText);
+			iterateJSON(mJson);
+		}
+	}
+	mRequest.open("GET", mUrl, true);
+	mRequest.send();
+	
+  }
+
+
+//function that will animate the arrows directions with css
+function toggleDetails(){
+	if($(".moreIndicator").hasClass("rot90")){
+		$(".moreIndicator").removeClass("rot90");
+		$(".moreIndicator").addClass("rot270");
+	} else {
+		$(".moreIndicator").removeClass("rot270");
+		$(".moreIndicator").addClass("rot90");
+	}
+	$(".details").slideToggle("slow", "linear");
+  }
